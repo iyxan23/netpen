502cd28f0b63f303f71d98c1cb8a5d4742a770fa
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-06 21:57:00 +0700

    feat: Implement login / register

--- app/src/main/java/com/iyxan23/netpen/LoginActivity.kt
+++ app/src/main/java/com/iyxan23/netpen/LoginActivity.kt
@@ -1,11 +1,52 @@
 package com.iyxan23.netpen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    fun login(view: View) {
+        val email: String = findViewById<TextInputEditText>(R.id.email_login).text.toString()
+        val password: String = findViewById<TextInputEditText>(R.id.password_login).text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@LoginActivity, it.message, Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun register(view: View) {
+        val email: String = findViewById<TextInputEditText>(R.id.email_login).text.toString()
+        val password: String = findViewById<TextInputEditText>(R.id.password_login).text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@LoginActivity, it.message, Toast.LENGTH_LONG).show()
+            }
     }
 }
